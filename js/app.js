61d4e20ff2605eb26c424ea138447129c922c7f8
Aydar Shakirov <shaaydar@yandex.ru>
2020-04-21 17:58:44 +0500

    работа с API

--- js/app.js
+++ js/app.js
@@ -13,56 +13,32 @@ const DOMAIN = 'http://tensor-school.herokuapp.com';
 
 const showPersonPage = function(PersonPage) {
     //получаем данные с сервера
-    // fetch(DOMAIN + 'user/current')
-    // .then(responce => responce.json()) //ждем ответ сервера
-    // .then(result => { //ждем обработку json
-    //     const personFromServer = result;
     
-    //     //временно, пока сервер не будет присылать коллекцию фото пользователя
-    //     personFromServer.data.photos = [
-    //         '/img/example/gallery/cat1.jpg',
-    //         '/img/example/gallery/cat2.jpg',
-    //         '/img/example/gallery/cat3.jpg',
-    //         '/img/example/gallery/cat4.jpg',
-    //     ];
+    fetch(DOMAIN + '/user/current', { credentials: 'include'})
+    .then(responce => responce.json()) //ждем ответ сервера
+    .then(result => { //ждем обработку json
+        const personFromServer = result;
+    
+        //временно, пока сервер не будет присылать коллекцию фото пользователя
+        personFromServer.data.photos = [
+            '/img/example/gallery/cat1.jpg',
+            '/img/example/gallery/cat2.jpg',
+            '/img/example/gallery/cat3.jpg',
+            '/img/example/gallery/cat4.jpg',
+        ];
     
     
-    //     const page = factory.create(PersonPage, {
-    //         person : personFromServer,
-    //     });
-    //     page.mount(document.body);
+        const page = factory.create(PersonPage, {
+            person : { 
+                ...personFromServer,
+                domain : DOMAIN,
+            },
+        });
+        page.mount(document.body);
     
-    //     document.title = `${personFromServer.data.name}`;
-    // })
-    // .catch(err => console.error(err));
-
-    const page = factory.create(PersonPage, {
-                person : {
-                    "id": 9,
-                    "data": {
-                        "job": "одмен",
-                        "city": "Уфа",
-                        "name": "Олег Макет",
-                        "education": "колледж информатики",
-                        "birth_date": "1998-07-07",
-                        "family_state": "есть сервак",
-                        photos : [
-                                    '/img/example/gallery/cat1.jpg',
-                                    '/img/example/gallery/cat2.jpg',
-                                    '/img/example/gallery/cat3.jpg',
-                                    '/img/example/gallery/cat4.jpg',
-                                ],
-                    },
-                    "computed_data": {
-                        "last_activity": "2020-04-20T19:29:59.086598",
-                        "photo_ref": "/img/example/gallery/cat1.jpg"
-                    }
-                },
-            });
-            page.mount(document.body);
-        
-            //document.title = `${personFromServer.data.name}`;
-    
+        document.title = `${personFromServer.data.name}`;
+    })
+    .catch(err => console.error(err));    
 };
 
 //роутинг на минималках
@@ -87,7 +63,8 @@ if(!document.cookie.match('sessionid=')){
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',
         },
-        body: formData.toString()
+        body: formData.toString(),
+        credentials : 'include'
     }).then(
         res => {
             if(res.status == 200){
