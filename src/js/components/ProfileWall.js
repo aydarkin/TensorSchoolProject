cc1646159c67cc395f0df00b9c0445a4b21ba048
shaaydar <shaaydar@yandex.ru>
2020-05-26 06:59:36 +0500

    добавление записей на стену

--- src/js/components/ProfileWall.js
+++ src/js/components/ProfileWall.js
@@ -9,11 +9,20 @@ define([
             super(options);
             this.setState({
                 person: this.options.person,
+                isSending: false,
             })
         }
 
         render() {
-            return `<div class="profile-wall"></div>`;
+            return `<div class="profile-wall">
+                        <div class="content__block post-create">
+                            <form class="post-create__form">
+                                <textarea class="post-create__text" name="text" id="" rows="2" placeholder="Что у вас нового?"></textarea>
+                                <button class="post-create__send">Опубликовать</button>
+                            </form>
+                        </div>
+                        <div class="profile-wall__posts"></div>
+                    </div>`;
         }
 
         clearPosts() {
@@ -28,6 +37,40 @@ define([
         afterMount() {
             this.clearPosts();
             this.loadPosts();
+
+            this.sendBtn = this.getContainer().querySelector('.post-create__send');
+            this.subscribeTo(this.sendBtn, 'click', this.clickSend.bind(this));
+
+            this.input = this.getContainer().querySelector('.post-create__text');
+            this.subscribeTo(this.input, 'keydown', this.keyDown.bind(this));
+        }
+
+        async createPost() {
+            this.setState({ isSending: true });
+            const message = await this.state.person.sendMessage(this.state.person.id, this.input.value);
+            const post = this.childrens.create(Post, {
+                person: this.state.person,
+                message: message.message,
+                images: [message.image],
+                openPhoto: this.options.openPhoto,
+            });
+            this.input.value = '';
+            const posts = this.getContainer().querySelector('.profile-wall__posts');
+            post.mount(posts, 'afterbegin');
+            this.setState({ isSending: false });
+        }
+
+        async clickSend(event) {
+            event.preventDefault();
+            if(!this.state.isSending && this.input.value.length > 0){
+                await this.createPost();
+            }  
+        }
+
+        async keyDown(event) {
+            if(!this.state.isSending && event.ctrlKey && event.key == 'Enter'){
+                await this.createPost();
+            }
         }
 
         async loadPosts() {
@@ -37,14 +80,18 @@ define([
                 const person = factory.create(PersonModel, message.author);
                 return this.childrens.create(Post, {
                     person: person,
-                    message : message.message,
-                    //date : message.date
+                    message: message.message,
+                    images: [message.image],
+                    openPhoto: this.options.openPhoto,
                 });
             });
 
+            const posts = this.getContainer().querySelector('.profile-wall__posts');
             children.forEach(child => {
-                child.mount(this.getContainer());
+                child.mount(posts);
             });
+
+
         }
     }
     return ProfileWall;
